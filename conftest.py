3d24c000b45e53575ae17bf86595070d3cbf31b1
Emilia99 <alekseenkoemilia@gmail.com>
2025-09-09 10:23:06 +0300

    chore: подготовка окружения

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language_name = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    browser.get(f"http://selenium1py.pythonanywhere.com/{language_name}/catalogue/coders-at-work_207/")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
