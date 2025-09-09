f7fb6a2aeb061df76ec7f994665c4777222ebe78
Emilia99 <alekseenkoemilia@gmail.com>
2025-09-09 21:03:13 +0300

    refactor: добавить методы проверки наличия форм и соответствия url в Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import time
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -7,3 +9,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    time.sleep(2)
+
+    page_login = LoginPage(browser, link)
+    page_login.should_be_login_url()
+    page_login.should_be_login_form()
+    page_login.should_be_register_form()
+    time.sleep(2)
