05e5f7fd5bb88c8c78c938bdeb0fcb455c711177
Emilia99 <alekseenkoemilia@gmail.com>
2025-09-11 11:06:11 +0300

    investigate: выявить падающий тест для промо-URL

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,15 @@
+import pytest
 from .pages.product_page import ProductPage
-from .pages.base_page import BasePage
 import time
 
 
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-def test_guest_can_add_product_to_basket(browser):
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_add_basket()
-    time.sleep(10)
+    time.sleep(5)
